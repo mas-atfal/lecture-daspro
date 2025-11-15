6acf91e2d18ddc0a078daaa0ad6954271338235b
DevFal <defal@mail.com>
2025-11-15 10:13:24 +0700

    feat: UTS2_00213

---
+++ UTS2_00213/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // REVERSE NUMBERS TRIANGLE
+
+    // FOR LOOP
+    // ==============================================
+    // for (int i = 5; i >= 1; i--) {
+    //     // 10 - 5 + 1
+    //     int start = 10 - i + 1;
+    //
+    //     for (int j = 10; j >= start  ; j--) {
+    //         cout << j << " ";
+    //     }
+    //
+    //     cout << endl;
+    // }
+
+    // WHILE LOOP
+    // ===============================================
+    // int baris = 5;
+    //
+    // while (baris >= 1) {
+    //     int angka = 10;
+    //     int start = angka - baris + 1;
+    //
+    //     while (angka >= start) {
+    //         cout << angka << " ";
+    //         angka--;
+    //     }
+    //
+    //     cout << endl;
+    //     baris --;
+    // }
+
+    // DO WHILE LOOP
+    // ===============================================
+    // int baris = 5;
+    //
+    // do {
+    //     int angka = 10;
+    //     int start = angka - baris + 1;
+    //
+    //     do {
+    //         cout << angka << " ";
+    //         angka--;
+    //     } while (angka >= start);
+    //
+    //     cout << endl;
+    //     baris--;
+    // } while (baris >= 1);
+
+    return 0;
+}
